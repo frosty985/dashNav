f59b229f0afe2eae65141a83a6dbb944ac29557f
frosty985 <35139571+frosty985@users.noreply.github.com>
2018-01-09 13:13:47 +0000

    Update canSend.py
    corrected keepalive signal

--- canSend.py
+++ canSend.py
@@ -11,9 +11,9 @@ uses:
 ''' requires trailing slash '''
 working_dir = "/tmp/"
 datafile    = "cansend.dat"
-logfile_dir = "/home/pi"
+logfile_dir = "/media/pi/DISK_IMG"
 logfile     = "cansend.log"
-can_interface = 'vcan0'
+can_interface = 'can0'
 can_refresh_rate = 0.1
 
 ''' Imports '''
@@ -40,7 +40,7 @@ def print_error(ex="Error", linenumber=0, message="There has been an error"):
     '''
     line = str(datetime.datetime.now()) + " @ " + str(linenumber) + " : " + str(ex) + " : " + str(message + "\n")
     try:
-        with open(logile_dir + "/" + logfile_name, "a") as error_file:
+        with open(logile_dir + logfile_name, "a") as error_file:
             error_file.write(line)
     except:
         print("ERROR WRITING TO FILE > ", line)
@@ -85,11 +85,28 @@ def send_message(id, message):
 
     bus.send(canMessage)
 
+def send_data(id, data):
+    '''
+    takes actual message and sends to the CAN BUS
+
+    :param id: takes a number, padds it out to 8 char with 0x at begining
+    :param message: actual string to send
+    :return: nothing
+    '''
+    #print(id)
+
+    #data = bytearray(data)
+    canMessage = can.Message(arbitration_id=id, data=data, extended_id=False)
+    #print(canMessage)
+
+    bus.send(canMessage)
+    
 def closing():
     print("CAN sender is closing")
     delay = 5
 
     while delay > 0:
+        send_data(0x00000661, [81, 1, 12, 38, 00, 00, 00, 00])
         send_message(0x00000363, "Exiting")
         send_message(0x00000365, "")
         time.sleep(can_refresh_rate)
@@ -102,14 +119,16 @@ if __name__ == '__main__':
     while True:
         try:
             time.sleep(can_refresh_rate)
-            with open(working_dir + "/" + datafile, "r") as canfile:
+            with open(working_dir + datafile, "r") as canfile:
                 #print("File loaded")
                 #print("Reset file searching delay")
                 timeout = 0
 
                 #print("Send stay alive message")
                 try:
-                    send_message(0x0000065f, "")
+                    send_data(0x00000661, [81, 1, 12, 38, 00, 00, 00, 00])
+                    #canMessage = can.Message(arbitration_id=0x00000661, data=81011238, extended_id=False)
+                    #bus.send(canMessage)
                 except Exception as ex:
                     tb = sys.exc_info()[2]
                     print_error(ex, tb.tb_lineno, "Error sending keep-alive to CAN")
